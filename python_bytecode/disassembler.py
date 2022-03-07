945c84282925e486e65882a2075ce2435cd8faaa
gustavomello9600 <gustavomelloenciv@gmail.com>
2022-03-07 08:46:47 -0300

    Primeiro programa em Scala

--- python_bytecode/disassembler.py
+++ python_bytecode/disassembler.py
@@ -1,7 +1,12 @@
 import sys
 from dis import dis
+from pathlib import Path
 
 filename = sys.argv[1]
+output_path = (Path(__file__).resolve().parent
+               / "disassembled_scripts"
+               / filename)
+output_path.mkdir(parents=True, exist_ok=True)
 
 pdf = False
 if len(sys.argv) >= 3:
@@ -10,13 +15,13 @@ if len(sys.argv) >= 3:
 with open(filename + ".py") as input_file:
     code = input_file.read()
 
-with open(filename + ".txt", "w") as output_file:
+with open(output_path / (filename + ".txt"), "w") as output_file:
     dis(code, file=output_file)
 
 if pdf:
     import os
 
-    with open(filename + ".txt") as output_file:
+    with open(output_path / (filename + ".txt")) as output_file:
         bytecode = output_file.read()
     markdown_output = f"""## Code
 ```{{.python .numberLines}}
@@ -31,4 +36,4 @@ if pdf:
     with open(filename + ".md", "w") as tmp:
         tmp.write(markdown_output)
 
-    os.system(f"pandoc {filename + '.md'} -o {filename + '.pdf'} -V fontsize=12pt && rm {filename + '.md'}")
+    os.system(f"pandoc {filename + '.md'} -o {output_path / (filename + '.pdf')} -V fontsize=12pt && rm {filename + '.md'}")
